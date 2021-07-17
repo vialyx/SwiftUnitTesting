2f3c8fa94c21bacb0171741aedac65ed26f7152b
Maksim Vyalykh <maksimvyalykh@coherentsolutions.com>
2021-07-17 18:52:16 +0200

    Initial Commit

---
+++ UnitTesting/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UnitTesting
+//
+//  Created by Maksim Vialykh on 7/17/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
